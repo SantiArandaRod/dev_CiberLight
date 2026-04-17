847b482906dbbfc789c3354f13040514a2eb949d
dsar <dsaranda89@ucatolica.edu.co>
2026-04-16 23:36:46 -0500

    el main ahora es seleccionar primero la db a usar

--- src/main/java/com/ciber/MainApp.java
+++ src/main/java/com/ciber/MainApp.java
@@ -1,7 +1,12 @@
 package com.ciber;
 
+import com.ciber.config.DatabaseConfig;
+import com.ciber.config.DatabaseType;
+import com.ciber.ui.DatabaseSelector;
+import com.ciber.util.DBInitializer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,11 +15,20 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        // 🔥 seleccionar DB antes de todo
+        DatabaseSelector.seleccionar();
+
+        if (DatabaseConfig.getDatabase() == DatabaseType.SQLITE) {
+            DBInitializer.initSQLite();
+        }
+
         FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("/view/main.fxml")
         );
 
-        Scene scene = new Scene(loader.load(), 720, 400);
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 720, 400);
 
         stage.setTitle("CiberLightCol - Sistema de Producción");
         stage.setScene(scene);
